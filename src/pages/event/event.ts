4eb7a5225583e8612cec8342558bcac0cf134ca2
lexis-nexus <alexispmarsh@gmail.com>
2018-05-04 01:13:38 -0400

    map api and geoloc

--- src/pages/event/event.ts
+++ src/pages/event/event.ts
@@ -3,10 +3,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { Event } from '../../models/event';
 import { Geolocation } from '@ionic-native/geolocation';
+// needed to access google API for geocoding
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 // import { GESTURE_PRIORITY_TOGGLE } from 'ionic-angular/gestures/gesture-controller';
 
-// hack to prevent errors
+// hack to prevent TypeScript errors
+declare var geocoder: any;
 declare var google: any;
+declare var map: any;
 
 @IonicPage()
 @Component({
@@ -18,47 +23,91 @@ export class EventPage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   event: Event;
-  coords: string;
+  geoWatch: any;
+  position: any = { lat: 0.0, lng: 0.0 };
+  has_pos: boolean;
+
+  pat_lat: number = 28.169163;
+  pat_lng: number = -80.646708;
+
+  geocodeURI: string = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+  google_key: string = '&key=AIzaSyACIn956WI7_FAS0cZwxHeqod_aK02FzcU';
 
   constructor(
+    public http: Http,
     public navCtrl: NavController,
     public navParams: NavParams,
     private geolocation: Geolocation) {
 
+      // assign data carried over from firebase
       this.event = this.navParams.data;
+      // this.RegisterPositionCallback(geolocation);
+      console.log(JSON.stringify(this.event));
   }
 
   ionViewDidLoad() {
-    console.log('beans beans beans');
-    this.geolocation.getCurrentPosition({
-      enableHighAccuracy: false,
-      timeout: 10000,
-      maximumAge: 15000
-    }).then((resp) => {
-      console.log('got position!');
-      this.loadMap(resp.coords.latitude, resp.coords.longitude);
-    }).catch((error) => {
-      console.log('Geolocation Error[' + error.code + '] ' + error.message);
-      this.loadMap(28.254215, -80.713872);
-    });
+    // build geocode api request uri
+    let uri = this.geocodeURI + this.event.location.address.replace(/ /g, '+') + this.google_key;
+    console.log(uri);
+    // hook geocode response
+    this.http.get(uri).map(res => res.json()).subscribe(
+      (data) => {
+        console.log(JSON.stringify(data));
+        if (data.status == 'OK') {
+          let loc = data.results[0].geometry.location;
+          console.log('ionViewDidLoad():loc = ' + JSON.stringify(loc));
+          this.position.lat = loc.lat;
+          this.position.lng = loc.lng;
+          this.loadMap();
+        } else {
+          this.position.lat = this.pat_lat;
+          this.position.lng = this.pat_lng;
+          console.log('Error during geocoding');
+          this.loadMap();
+        }
+      }, (error) => {
+        console.log('geocoding[' + error.code + '] ' + error.message);
+        this.position.lat = this.pat_lat;
+        this.position.lng = this.pat_lng;
+        this.loadMap();
+      }
+    );
   }
 
-  loadMap(latitude: number, longitude: number) {
-    console.log('loadMap()...');
-    let latLng = new google.maps.LatLng(latitude, longitude);
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave() in pages/event/event.ts');
+    // deregister callbacks
+    // this.geoWatch.unsubscribe();
+  }
+
+  loadMap() {
+    console.log('loadMap() in pages/event/event.ts');
+    // construct settings object
+    let latLng = new google.maps.LatLng(this.position.lat, this.position.lng);
     let mapOptions = {
       center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      zoom: 10,
+      animation: google.maps.Animation.DROP
     };
+    // build map using api
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-    let marker = new google.maps.Marker({
+
+    var marker = new google.maps.Marker({
+      position: latLng,
       map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: this.map.getCenter(),
-      fullscreenControl: false
+      title: this.event.location.name
     });
-    // hack to prevent warnings
     console.log(marker);
+  }
+
+  RegisterPositionCallback(geolocation: Geolocation) {
+    this.geoWatch = geolocation.watchPosition();
+    this.geoWatch.subscribe(this.WatchPositionCallback);
+  }
+
+  WatchPositionCallback(data: any) {
+    if (data.coords) {
+
+    }
   }
 }
